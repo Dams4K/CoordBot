9fbf0fd8609a87e8944fc7131c2b817fb1715514
Dams4K <dams4k@users.noreply.github.com>
2023-03-17 18:48:51 +0100

    slightly better ranking formatter

--- cogs/commands/ranking.py
+++ cogs/commands/ranking.py
@@ -1,9 +1,60 @@
 import discord
 from discord.ext import commands, bridge
 from data_management import *
-from utils.bot_embeds import NormalEmbed
 from operator import attrgetter
 from prefixed import Float
+from utils.bot_embeds import NormalEmbed
+
+class FormatterDict(dict):
+    def __missing__(self, key):
+        return "{" + key + "}"
+
+class RankingFormatter:
+    def __init__(self, competitors, sort_attrs: list):
+        self.competitors = competitors
+        self.sort_attrs = sort_attrs
+    
+    def get_ranking_list(self) -> dict:
+        sorted_competitors: list = sorted(self.competitors, key=attrgetter(*self.sort_attrs), reverse=True)
+        
+        ranking: dict = {}
+        last_position = None
+        last_attrs = [None] * len(self.sort_attrs)
+        for i in range(len(sorted_competitors)):
+            competitor = sorted_competitors[i]
+            
+            position = i+1
+            current_attrs = [getattr(competitor, attr_name) for attr_name in self.sort_attrs]
+            if last_attrs == current_attrs:
+                position = last_position
+            
+            last_position = position
+            last_attrs = current_attrs
+
+            ranking.setdefault(position, [])
+            ranking[position].append(competitor)
+
+        return ranking
+    
+    def get_ranking_string(self, str_format: str, optional: set = {}, **kwargs: dict) -> str:
+        ranking: dict = self.get_ranking_list()
+
+        str_list: list = []
+        for i in range(len(ranking)):
+            position = list(ranking.keys())[i]
+            for competitor in ranking[position]:
+                format_dict = {attr_name: getattr(competitor, attr_name) for attr_name in self.sort_attrs}
+                format_dict.update({
+                    "competitor": competitor,
+                    "pos": position
+                })
+                format_dict.update({k: v(competitor) for k, v in kwargs.items() if callable(v)})
+
+                str_list.append(str_format.format_map(FormatterDict(format_dict)))
+        
+        return "\n".join(str_list)
+
+
 
 class RankingCog(commands.Cog):
     def __init__(self, bot):
@@ -18,24 +69,17 @@ class RankingCog(commands.Cog):
         members_data = [MemberData(member.id, ctx.guild.id) for member in ctx.guild.members]
         default_member = GuildDefaultMemberData(ctx.guild.id)
         members_data = list(filter(lambda md: md.level > default_member.level or md.xp > default_member.xp, members_data))
-        members_data.sort(key=attrgetter("level", "xp"), reverse=True)
+
+        ranking_formatter = RankingFormatter(members_data, ("level", "xp"))
+
+        get_competitor_name = lambda c: discord.utils.find(lambda m: m.id == c._member_id, ctx.guild.members)
+        get_level = lambda c: f"{Float(c.level):.2h}" if len(str(c.level)) > 3 else c.level
+        get_xp = lambda c: f"({Float(c.xp):.2h})" if len(str(c.xp)) > 3 else f"({c.xp})"
         
-        formatted_top = []
-        last_ranking, last_level, last_xp = None, None, None
-        for i in range(0, min(15, len(members_data))):
-            member_data = members_data[i]
-            ranking = i+1
-
-            if last_level == member_data.level and last_xp == member_data.xp:
-                ranking = last_ranking
-
-            last_ranking, last_level, last_xp = ranking, member_data.level, member_data.xp
-
-            member = discord.utils.find(lambda m: m.id == member_data._member_id, ctx.guild.members)
-            formatted_top.append(f"{ranking}. {member.name if member is not None else None}: {member_data.level} ({Float(member_data.xp):.2h})")
-
+        ranking_str = ranking_formatter.get_ranking_string("{pos}. {competitor_name.name}: {level} {xp}", optional={"xp"}, competitor_name=get_competitor_name, level=get_level, xp=get_xp)
+        
         embed = NormalEmbed(ctx.guild_config, title="Top 15 levels")
-        embed.description = "\n".join(formatted_top) if formatted_top != [] else ctx.translate("NOBODY_IN_RANKING")
+        embed.description = ranking_str if ranking_str != "" else ctx.translate("NOBODY_IN_RANKING")
         await ctx.respond(embed=embed)
     
     @ranking.command(name="money")
@@ -43,24 +87,16 @@ class RankingCog(commands.Cog):
         members_data = [MemberData(member.id, ctx.guild.id) for member in ctx.guild.members]
         default_member = GuildDefaultMemberData(ctx.guild.id)
         members_data = list(filter(lambda md: md.money > default_member.money, members_data))
-        members_data.sort(key=attrgetter("money"), reverse=True)
+        
+        ranking_formatter = RankingFormatter(members_data, ("money",))
 
-        formatted_top = []
-        last_ranking, last_money = None, None
-        for i in range(0, min(15, len(members_data))):
-            member_data = members_data[i]
-            ranking = i+1
+        get_competitor_name = lambda c: discord.utils.find(lambda m: m.id == c._member_id, ctx.guild.members)
+        get_money = lambda c: f"{Float(c.money):.2h}" if len(str(c.money)) > 3 else c.money
 
-            if last_money == member_data.money:
-                ranking = last_ranking
-            
-            last_ranking, last_money = ranking, member_data.money
-
-            member = discord.utils.find(lambda m: m.id == member_data._member_id, ctx.guild.members)
-            formatted_top.append(f"{ranking}. {member.name if member is not None else None}: {Float(member_data.money):.2h}")
+        ranking_str = ranking_formatter.get_ranking_string("{pos}. {competitor_name.name}: {money}", competitor_name=get_competitor_name, money=get_money)
 
         embed = NormalEmbed(ctx.guild_config, title="Top 15 money")
-        embed.description = "\n".join(formatted_top) if formatted_top != [] else ctx.translate("NOBODY_IN_RANKING")
+        embed.description = ranking_str if ranking_str != "" else ctx.translate("NOBODY_IN_RANKING")
         await ctx.respond(embed=embed)
 
 def setup(bot):
