015aeaef9bfdf4fe8aec365cd34faa91206420f1
Dams4K <dams4k@users.noreply.github.com>
2023-07-25 12:39:09 +0200

    more activities

--- cogs/tasks/varying_presence.py
+++ cogs/tasks/varying_presence.py
@@ -2,9 +2,14 @@ from discord import *
 from discord.ext import tasks
 
 class VaryingPresence(Cog):
+    # I added the URLs for whoever wants to hear these songs
     ACTIVITIES = [
         Activity(name="OMORI - It Means Everything [lofi hiphop/chillhop remix]", type=ActivityType.listening, url="https://www.youtube.com/watch?v=88DcHP-wEyY"),
-        Activity(name="Machinarium Soundtrack 00 - By the Wall", type=ActivityType.listening, url="https://www.youtube.com/watch?v=jex5rtwx94k&ab_channel=GoodMomo")
+        Activity(name="Machinarium Soundtrack 00 - By the Wall", type=ActivityType.listening, url="https://www.youtube.com/watch?v=jex5rtwx94k"),
+        Activity(name="Senbeï & Proleter - Lady Vengeance", type=ActivityType.listening, url="https://www.youtube.com/watch?v=DkzH3RflIPQ"),
+        Activity(name="Imaginary Interlude - C418", type=ActivityType.listening, url="https://www.youtube.com/watch?v=ImCFqNNvXTs"),
+        Activity(name="Monodrama - Ignant Benches", type=ActivityType.listening, url="https://www.youtube.com/watch?v=vL0OtwGHGXE&ab_channel=IgnantBenches-Topic"),
+        Game(name="Tunic"),
     ]
 
     def __init__(self, bot) -> None:
