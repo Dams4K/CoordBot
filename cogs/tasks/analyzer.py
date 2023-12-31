770408fe38549042bf4eb5ebb0d4449091ce804f
Dams4K <dams4k@users.noreply.github.com>
2023-12-31 18:00:28 +0100

    disable print of analyzer

--- cogs/tasks/analyzer.py
+++ cogs/tasks/analyzer.py
@@ -15,7 +15,8 @@ class AnalyzerCog(Cog):
 
     @tasks.loop(seconds=0.01)
     async def analyze(self):
-        print(self.get_total_data_size(GuildConfig, GuildLanguage, GuildSalaries, GuildLevelingConfig, MemberData, GuildArticle, GuildObject))
+        # print(self.get_total_data_size(GuildConfig, GuildLanguage, GuildSalaries, GuildLevelingConfig, MemberData, GuildArticle, GuildObject))
+        pass
 
     def get_total_data_size(self, *data_classes):
         total = 0
