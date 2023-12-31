4343b4e530152459d7aad57dd6b4bb1c89ccd237
Dams4K <dams4k@users.noreply.github.com>
2023-12-31 17:58:30 +0100

    eeeee

--- cogs/tasks/analyzer.py
+++ cogs/tasks/analyzer.py
@@ -4,7 +4,6 @@ from discord.ext import tasks
 from data_management import *
 
 from pympler.asizeof import asizeof
-import sys
 
 class AnalyzerCog(Cog):
     def __init__(self, bot):
@@ -14,17 +13,20 @@ class AnalyzerCog(Cog):
     async def on_ready(self):
         self.analyze.start()
 
-    @tasks.loop(seconds=0.1)
+    @tasks.loop(seconds=0.01)
     async def analyze(self):
-        print(asizeof(MemberData.instances), MemberData.instances)
-        
-        # ids_to_del = []
-        # for inst_id, weak_inst in MemberData.instances.items():
-        #     if weak_inst() is None:
-        #         ids_to_del.append(inst_id)
+        print(self.get_total_data_size(GuildConfig, GuildLanguage, GuildSalaries, GuildLevelingConfig, MemberData, GuildArticle, GuildObject))
 
-        # for inst_id in ids_to_del:
-        #     MemberData.instances.pop(inst_id)
+    def get_total_data_size(self, *data_classes):
+        total = 0
+        for clazz in data_classes:
+            # print(clazz, clazz is Saveable)
+            # if not isinstance(clazz, Saveable):
+            #     continue
+            total += asizeof(clazz.instances)
+
+        return total
+
         
 
 def setup(bot):
