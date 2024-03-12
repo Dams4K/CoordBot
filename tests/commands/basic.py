4b4bc436e53c0d047ba25b22d1cbf5b954f27849
Dams4K <dams4k@users.noreply.github.com>
2024-03-12 11:25:37 +0100

    re enable terminal & removing useless files

--- tests/commands/basic.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestBasicCog(unittest.IsolatedAsyncioTestCase):
-    async def test_profil(self):
-        pass
-        # await basicCog.slash_profil(TestApplicationContext(bot, None))
-
-if __name__ == "__main__":
-    unittest.main()
